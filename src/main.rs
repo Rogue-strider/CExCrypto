b89a17e83f7bd092d33cc0d8fa820bdae25bc3ea
Rogue-strider <satyamjha91064@gmail.com>
2026-02-13 22:44:03 +0530

    Creating order route

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, delete, get, post};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, delete, get, post};
+use serde::{Deserialize, Serialize};
+
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -13,17 +15,45 @@ async fn main() -> Result<(), std::io::Error> {
     .await
 }
 
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CreateOrder {
+    pub price: f64,
+    pub quantity: f64,
+    pub user_id: String,
+    pub side: Side,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Side {
+    Buy,
+    Sell
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DeleteOrder {
+    pub order_id: String,
+    pub user_id: String,
+}
+
 #[post("/order")]
-async fn create_order() -> impl Responder {
-    HttpResponse::Ok().body("Order created")
+async fn create_order(order: web::Json<CreateOrder>) -> impl Responder {
+    println!("Received order: {:?}", order);
+
+    HttpResponse::Ok().json("Order created")
 }
 
 #[delete("/order")]
-async fn delete_order() -> impl Responder {
-    HttpResponse::Ok().body("Order deleted")
+async fn delete_order(order: web::Json<DeleteOrder>) -> impl Responder {
+    println!("Delete order: {:?}", order);
+
+    HttpResponse::Ok().json("Order deleted")
 }
+
 
 #[get("/depth")]
 async fn get_depth() -> impl Responder {
     HttpResponse::Ok().body("Market depth")
 }
+//1.11
